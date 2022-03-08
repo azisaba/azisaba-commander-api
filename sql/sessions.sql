aa4acd343cca8f516ed101528b4ce37e64201829
testusuke <silverbro007@icloud.com>
2022-03-08 19:33:26 +0900

    change: add 'pending' variable

--- sql/sessions.sql
+++ sql/sessions.sql
@@ -4,6 +4,7 @@ create table sessions
     expires_at bigint       not null,
     user_id    int          not null,
     ip         varchar(255) not null,
+    pending    tinyint(1)   DEFAULT 0,
     constraint sessions_pk
         primary key (state)
 );

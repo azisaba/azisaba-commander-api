aeba4640d6cc04b0697f41bdd55dca17652bfc81
testusuke <silverbro007@icloud.com>
2022-03-11 15:32:59 +0900

    change: allow null

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,10 +1,10 @@
 create table logs
 (
     `id`            int auto_increment,
-    `user_id`       int             not null,
-    `area`          varchar(255)    not null,
-    `action`        varchar(255)    not null,
-    `description`   varchar(512)    not null,
+    `user_id`       int             null,
+    `area`          varchar(255)    null,
+    `action`        varchar(255)    null,
+    `description`   varchar(512)    null,
     `date`          DATETIME default CURRENT_TIMESTAMP null,
     constraint logs_pk
         primary key (`id`)

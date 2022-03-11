ca08cd43a4008d0e5f2e9c78dadde8991ad1881f
testusuke <silverbro007@icloud.com>
2022-03-11 15:11:45 +0900

    change: use project/service name for identification

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,8 +1,9 @@
 create table permissions
 (
-    id          int auto_increment,
-    name        varchar(255) not null,
-    target_name varchar(255) not null,
+    `id`        int auto_increment,
+    `name`      varchar(255) not null,
+    `project`   varchar(255) not null,
+    `service`   varchar(255) not null,
     constraint permissions_pk
         primary key (id)
 );

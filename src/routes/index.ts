69a5adfbd3311c92e25ef3913b5868e02a3d425f
testusuke <silverbro007@icloud.com>
2022-04-13 15:01:31 +0900

    feat: add routers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,15 @@
-import express from "express";
+import express from "express"
+import {router as registerRouter} from "./register"
+import {router as loginRouter} from "./login"
+import {router as logoutRouter} from "./logout"
+import {router as meRouter} from "./me"
 
 export const router = express.Router();
+
+router.use('/register', registerRouter)
+router.use('/login', loginRouter)
+router.use('/logout', logoutRouter)
+router.use('/me', meRouter)
 
 
 router.get('/', (req, res) => {

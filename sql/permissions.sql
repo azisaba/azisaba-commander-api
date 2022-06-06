b453b8111859982ed481ee72e7c79d75e3908e0f
testusuke <silverbro007@icloud.com>
2022-06-06 15:32:58 +0900

    change: change colum name

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -2,8 +2,7 @@ create table permissions
 (
     `id`        int auto_increment,
     `name`      varchar(255) not null,
-    `project`   varchar(255) not null,
-    `service`   varchar(255) not null,
+    `content`   text         not null,
     constraint permissions_pk
         primary key (`id`)
 );

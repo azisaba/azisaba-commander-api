29df9d962596cbde218d8d047b85ce8fc1201959
testusuke <silverbro007@icloud.com>
2022-06-09 20:50:00 +0900

    change: add functions(get,delete,create)

--- src/util/permission.ts
+++ src/util/permission.ts
@@ -1,5 +1,11 @@
 import * as sql from "./sql"
 
+/**
+ * Get a permission info
+ *
+ * @param id
+ * @return Permission
+ */
 export const getPermission = async (id: number): Promise<Permission | null> => {
     const p = await sql.findOne(
         "SELECT * FROM `permissions` WHERE `id`=?",
@@ -15,6 +21,52 @@ export const getPermission = async (id: number): Promise<Permission | null> => {
     }
 }
 
+/**
+ * Get all permission info
+ *
+ * @return Array<Permission>
+ */
+export const getAllPermission = async (): Promise<Array<Permission> | null> => {
+    const p = await sql.findOne(
+        "SELECT * FROM `permissions`"
+    )
+    //  if not find, return null
+    if (!p || typeof p !== 'object') return null
+
+    const permissions = new Array<Permission>()
+    for (let permission of p) {
+        //  parse
+        permission.content = parseContent(permission.content)
+
+        permissions.push(permission)
+    }
+
+    return permissions
+}
+
+export const deletePermission = async (id: number): Promise<void> => {
+    //  delete from sql
+    await sql.execute(
+        "DELETE FROM `permissions` WHERE `id`=?",
+        id
+    )
+    return
+}
+
+export const createPermission = async (permission: Permission): Promise<number | null> => {
+    //  format content
+    const contentStr = formatContent(permission.content)
+
+    //  sql
+    const id = await sql.findOne(
+        "INSERT INTO `permissions` (`name`, `content`) VALUES (?, ?)",
+        permission.name,
+        contentStr
+    ) as number
+
+    if (!id) return null
+    return id
+}
 
 /**
  * Parse permission content

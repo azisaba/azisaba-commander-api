680b6dc02afe92b96a4f3bd70b2265e68fca4864
testusuke <silverbro007@icloud.com>
2022-06-16 23:25:17 +0900

    change: add something...

--- src/util/users.ts
+++ src/util/users.ts
@@ -1,6 +1,6 @@
 import * as sql from "./sql";
 import {GROUP_ADMIN} from "./constants";
-import {getPermission} from "./permission";
+import * as permissionTs from "./permission";
 import * as crypto from "./crypto";
 
 /**
@@ -13,34 +13,34 @@ export const getAllUser = async (): Promise<Array<User>> => {
 
 /**
  * Get user profile from id
- * @param id
+ * @param userId
  * @return User
  */
-export const getUser = async (id: number): Promise<User | null> => {
-    return await sql.findOne('SELECT `id`, `username`, `group` FROM `users` WHERE `id`=?', id)
+export const getUser = async (userId: number): Promise<User | null> => {
+    return await sql.findOne('SELECT `id`, `username`, `group` FROM `users` WHERE `id`=?', userId)
 }
 
 /**
  * change user password
- * @param id
+ * @param userId
  * @param password
  * @return Boolean
  */
-export const changePassword = async (id: number, password: string): Promise<void> => {
+export const changePassword = async (userId: number, password: string): Promise<void> => {
     return await sql.execute(
         "UPDATE `users` SET `password`=? WHERE `id`=?",
         await crypto.hash(password),
-        id
+        userId
     )
 }
 
 /**
  *  check if user is admin group
- *  @param id
+ *  @param userId
  *  @return boolean
  */
-export const isAdmin = async (id: number): Promise<boolean> => {
-    const user = await getUser(id)
+export const isAdmin = async (userId: number): Promise<boolean> => {
+    const user = await getUser(userId)
     return !(!user || user.group !== GROUP_ADMIN)
 }
 
@@ -49,7 +49,7 @@ export const isAdmin = async (id: number): Promise<boolean> => {
  *  @param userId
  *  @return Array<number>
  */
-export const getAllUserPermissionId = async (userId: number): Promise<Array<number> | null> => {
+export const getAllPermissionId = async (userId: number): Promise<Array<number> | null> => {
     const list = await sql.findAll('SELECT `permission_id` FROM `users_permission` WHERE `user_id`=?', userId)
     if (typeof list !== 'object') return null
     return list.map(r => r.permission_id)
@@ -59,16 +59,44 @@ export const getAllUserPermissionId = async (userId: number): Promise<Array<numb
  *  get all permission contents
  *
  */
-export const getAllUserPermission = async (userId: number): Promise<Array<Permission> | null> => {
-    const ids = await getAllUserPermissionId(userId)
+export const getAllPermission = async (userId: number): Promise<Array<Permission> | null> => {
+    const ids = await getAllPermissionId(userId)
     if (!ids) return null
 
     const permissions = new Array<Permission>()
     for (const id of ids) {
-        const permission = await getPermission(id)
+        const permission = await permissionTs.get(id)
         if (!permission) continue
         permissions.push(permission)
     }
 
     return permissions
+}
+
+export const getAllPermissionContents = async (userId: number): Promise<Array<PermissionContent> | null> => {
+    const permissions = await getAllPermission(userId)
+    if (!permissions) return null
+
+    let contents = new Array<PermissionContent>()
+    for (const permission of permissions) {
+        contents.concat(permission.content)
+    }
+    return contents
+}
+
+export const hasPermission = async (userId: number, permissionContent: PermissionContent): Promise<Boolean> => {
+    const contents = await getAllPermissionContents(userId)
+    if (!contents) return false
+
+    return contents.some((content) => {
+        //  project
+        if (content.project !== "*" && content.project !== permissionContent.project) {
+            return false
+        }
+        //  service
+        if (content.service !== "*") {
+            return false
+        }
+        //  todo 条件
+    })
 }

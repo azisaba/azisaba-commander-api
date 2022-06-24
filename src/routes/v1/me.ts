1395bf4de06772bf1d52ed07e3c3191c348bde89
testusuke <silverbro007@icloud.com>
2022-06-24 10:04:44 +0900

    change: add permission,2fa content

--- src/routes/v1/me.ts
+++ src/routes/v1/me.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import {validateAndGetSession} from "../../util/util";
 import * as sql from "../../util/sql";
+import * as userUtil from "../../util/users";
+import * as twoFA from "../../util/2fa";
 
 const debug = require('debug')('azisaba-commander-api:route:v1:me')
 export const router = express.Router();
@@ -17,6 +19,15 @@ router.get('/', async (req, res) => {
     if (!session) return res.status(404).send({error: 'not_found'})
     const user = await sql.findOne("SELECT `id`, `username`, `group`, `last_update` FROM `users` WHERE `id` = ?", session.user_id)
     if (!user) return res.status(401).send({error: 'unauthorized'})
-    //  todo return permission, and other profiles.
-    res.status(200).send(user)
+
+    const permissions = await userUtil.getAllPermission(user.id)
+    const is2Fa = await twoFA.isRegistered(user.id)
+
+    res.status(200).send(
+        {
+            ...user,
+            "permissions": permissions,
+            "2fa": is2Fa
+        }
+    )
 })

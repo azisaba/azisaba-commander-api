fff6b52603ec87924d6e141c75c66e3afd45c87f
testusuke <silverbro007@icloud.com>
2022-07-01 23:34:19 +0900

    change: register v1 router

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const debug = require('debug')('azisaba-commander-api:app')
 import express from 'express'
+import {generateSaltRounds} from "./util/crypto";
 import cors from 'cors'
 import logger from 'morgan'
 import * as sql from './util/sql'
 import * as docker from './util/docker'
+import cookieParser from "cookie-parser";
+
+//  router: v1
+import {router as indexV1Router} from "./routes/v1";
 
 //  init module
 sql.init()
     .then(async () => {
-        //  TODO something
+        //  generate salt
+        const rounds = await generateSaltRounds()
+        debug('Generated salt rounds: %d', rounds)
 
+        //  init docker loader
         await docker.init()
     })
     // @ts-ignore
@@ -31,4 +39,9 @@ app.use(cors(corsOptions));
 //  json, url encoder
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
+//  todo create not found direction
+
+//  router
+app.use('/', indexV1Router)

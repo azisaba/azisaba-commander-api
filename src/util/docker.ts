3308fbf190126374264e1608f62b43d4c5be73c4
testusuke <silverbro007@icloud.com>
2022-07-12 23:24:03 +0900

    change: add some function

--- src/util/docker.ts
+++ src/util/docker.ts
@@ -1,50 +1,15 @@
-import Docker from 'dockerode'
+import Docker, {Container} from 'dockerode'
 import {config} from './config'
 import {sleep} from "./util";
+import Dockerode from "dockerode";
 const debug = require('debug')('azisaba-commander-api:docker')
 
 const _nameDockerMap = new Map<string, Docker>();
-
-/**
- * get map of docker instances
- * @return Map<string, Docker>
- */
-export const dockerMap = (): Map<string, Docker> => {return _nameDockerMap}
-
-/**
- * get Docker instance by name
- * @param name docker name
- * @return Docker|undefined
- */
-export const getDocker = (name: string): Docker | undefined => {return _nameDockerMap.get(name)}
 
 export const init = async () => {
     if (!config['docker']) {
         debug("config have no docker setting")
         return
-    }
-
-    //  connection test and insert it
-    const insertDocker = async (name: string, docker: Docker): Promise<void> => {
-        try {
-            //  todo after debug, pls set 6000
-            await Promise.race([sleep(1000), docker.ping()]).then(result => {
-                //  time-out
-                if (!result) {
-                    debug('Error: %s is timed out', name)
-                }
-
-                if (String(result) === "OK") {
-                    debug('established connection to %s', name)
-                    //  insert
-                    _nameDockerMap.set(name, docker)
-                } else {
-                    debug('Error: something wrong. name: %s', name)
-                }
-            })
-        } catch (e) {
-            debug('Error: occurred exception during ping pong. name: %s', name)
-        }
     }
 
     for (const key in config['docker']) {
@@ -75,7 +40,7 @@ export const init = async () => {
                 })
 
                 //  inspect and insert docker
-                await insertDocker(value['name'], docker)
+                await inspectDockerode(value['name'], docker)
                 break
             }
 
@@ -93,13 +58,89 @@ export const init = async () => {
                 })
 
                 //  inspect and insert docker
-                await insertDocker(value['name'], docker)
+                await inspectDockerode(value['name'], docker)
                 break
             }
 
             default: {
                 debug('this protocol does not support. protocol: %s', protocol)
                 break
+            }
+        }
+    }
+}
+
+/**
+ * Inspect docker instance with ping-pong. inset it into internal map if pass inspection.
+ *
+ * @param name
+ * @param docker
+ */
+const inspectDockerode = async (name: string, docker: Dockerode) => {
+    try {
+        //  todo after debug, pls set 6000
+        await Promise.race([sleep(3000), docker.ping()]).then(result => {
+            //  time-out
+            if (!result) {
+                debug('Error: %s is timed out', name)
+            }
+
+            if (String(result) === "OK") {
+                debug('established connection to %s', name)
+                //  insert
+                _nameDockerMap.set(name, docker)
+            } else {
+                debug('Error: something wrong. name: %s', name)
+            }
+        })
+    } catch (e) {
+        debug('Error: occurred exception during ping pong. name: %s', name)
+    }
+}
+
+// getter
+
+/**
+ * get Docker instance by name
+ * @param name docker name
+ * @return Docker|undefined
+ */
+export const getDocker = (name: string): Docker | undefined => {return _nameDockerMap.get(name)}
+
+/**
+ * Get all container
+ *
+ * @return Promise<Array<Container>>
+ */
+export const getAllContainer = async (): Promise<Array<Container>> => {
+    const list = new Array<Container>()
+
+    for (const [name, node] of _nameDockerMap) {
+        const nodeInfo = await node.info()
+        const containers = await node.listContainers()
+        for (const container of containers) {
+            const containerInspection = await node.getContainer(container.Id).inspect();
+            const containerStats = await node.getContainer(container.Id).stats();
+            const formattedContainer = {
+                id: container.Id,
+                docker_id: nodeInfo.ID,
+                name: name,
+                created_at: containerInspection.Created,
+                project_name: container.Labels['com.docker.compose.project'],
+                service_name: container.Labels['com.docker.compose.service'],
+                status: {
+                    state: {
+                        state: container.State,
+                        status: container.Status,
+                        started_at: containerInspection.State.StartedAt,
+                        finished_at: containerInspection.State.FinishedAt
+                    },
+                    network_state: {
+
+                    },
+                    memory_states: {},
+                    cpu_states: {}
+                }
             }
         }
     }

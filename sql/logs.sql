6415c934fa0d148c7b17511432925f3cd24af87a
testusuke <silverbro007@icloud.com>
2022-07-29 13:37:08 +0900

    change: update logs table

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,11 +1,9 @@
 create table logs
 (
-    `id`            int auto_increment,
-    `user_id`       int             null,
-    `area`          varchar(255)    null,
-    `action`        varchar(255)    null,
-    `description`   varchar(512)    null,
-    `date`          DATETIME default CURRENT_TIMESTAMP null,
+    `id`        int     auto_increment,
+    `user_id`   int     null,
+    `message`   TEXT    null,
+    `date`      DATETIME default CURRENT_TIMESTAMP null,
     constraint logs_pk
         primary key (`id`)
 );
